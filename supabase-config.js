ab24dcf78ee5c2030da00a6dd3bd92e89f1be396
moisesabigail22 <moisesabigail22@gmail.com>
2026-03-02 04:40:56 -0400

    Configurar anon public key de Supabase en frontend

--- supabase-config.js
+++ supabase-config.js
@@ -5,5 +5,5 @@ window.EPIC_SUPABASE_CONFIG = {
   url: 'https://TU-PROYECTO.supabase.co',
 
   // Publishable key (segura para navegador con RLS activo)
-  anonKey: 'sb_publishable_d8czXDE3zG8BiVgah6NilA_MuHAyp25'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5aHBtanNtbnZlZmxsdHFnYWtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNzU1OTAsImV4cCI6MjA4NzY1MTU5MH0.IpDNLw4XpMDA5VTrLFVyYpeClXiX_uKjRSux7qNIIwU'
 };
